b24d735f2f7a248b6440efcd5342d3bd23e88bb7
default <clement.martinet98@gmail.com>
2022-06-21 12:18:37 +0200

    enregistrement via myges

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 namespace App\Repository;
 
 use App\Entity\User;
+use GuzzleHttp\Client as HTTPClient;
 
 final class UserRepository extends AbstractRepository
 {
@@ -12,4 +13,49 @@ final class UserRepository extends AbstractRepository
     $user = $user->get();
     return !empty($user['id']) ?  true : false;
   }
+
+
+  /**
+     * Get current profile
+     *
+     * @return array
+     */
+    public static function getProfile($login, $mdp)
+    {
+      //récupération token
+      $url = "https://authentication.kordis.fr/oauth/authorize?client_id=skolae-app&response_type=token";
+      $client = new HTTPClient();
+      $response = $client->request('GET', $url, [
+          'auth' => [
+              $login,
+              $mdp
+          ],
+          'allow_redirects' => false,
+          'http_errors' => false,
+          'verify' => false,
+
+      ]);
+      $headers = $response->getHeaders();
+      if($response->getStatusCode() == 401){
+        return false;
+      }
+      $location = $headers['Location'][0];
+      $locationUrl = parse_url($location);
+      parse_str($locationUrl['fragment'], $queryParams);
+
+      //récupération infos du profile
+      $url = "https://api.kordis.fr/me/profile";
+      $curl = curl_init($url);
+      $headers = array(
+        "Authorization: Bearer " . $queryParams['access_token']
+     );
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+      $resp = curl_exec($curl);
+      curl_close($curl);
+      return json_decode($resp, true);
+    }
 }

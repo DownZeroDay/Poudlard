e7440fb07de65be4a3dbd86ac59715a0e32b328e
MartinetC <71493398+MartinetCl@users.noreply.github.com>
2022-06-23 18:41:09 +0200

    change header v3

--- src/Controller/BdeController.php
+++ src/Controller/BdeController.php
@@ -1,10 +1,8 @@
 <?php
-/* A modifier avec access control */
+
 namespace App\Controller;
 
-use App\Entity\Catevenement;
 use App\Routing\Attribute\Route;
-use App\Entity\Evenement;
 use App\Repository\CategorieRepository;
 use App\Repository\EvenementRepository;
 use App\Repository\UserRepository;
@@ -13,34 +11,34 @@ class BdeController extends AbstractController
 {
     /** Methode qui permet d'afficher les infos du BDE */
     #[Route(path: '/infos_bde',  httpMethod: ['GET'], name: 'infos_bde')]
-    public function infos_bde(UserRepository $reposUser, EvenementRepository $repoEvent, 
-        CategorieRepository $repoCat, UserRepository $repoUsers){
-
+    public function infos_bde(
+        UserRepository $reposUser,
+        EvenementRepository $repoEvent,
+        CategorieRepository $repoCat,
+        UserRepository $repoUsers
+    ) {
+        if (!empty($_SESSION)) {
+            $this->params['session'] = $_SESSION;
+        }
         $users = $reposUser->findBy(1);
         $evenements = $repoEvent->findByCat();
         $categories = $repoCat->findAll();
         $users      = $reposUser->findAll();
-
-        echo $this->twig->render('index/infos_bde.html.twig', [
-            'users' => $users, 
-            'evenements' => $evenements, 
-            'categories' => $categories
-        ]);
+        $this->params['users'] = $users;
+        $this->params['evenements'] = $evenements;
+        $this->params['categories'] = $categories;
+        $this->params['title'] = 'BDE';
+        $this->views = [['index/infos_bde.html.twig', 0]];
+        $this->viewPage();
     }
 
     /** Methode qui permet de supprimer un evenement */
-    
-    #[Route(path: '/delete_event/{id}',  httpMethod: ['POST'], name: 'delete_event')]
-    public function delete_event($id, EvenementRepository $repoEvent){
 
-        // $evenement = new Evenement($id);
-        // $evenement = $evenement->get();
-        var_dump($repoEvent->deleteById($id));
-        // if(isset($_POST['linkdelete']) == "POST"){
-        //     var_dump($repoEvent->deleteById($id));
-        // }
-        if($repoEvent->deleteById($id)){
-            echo("Supprimer avec succes");
+    #[Route(path: '/delete_event/{id}',  httpMethod: ['POST'], name: 'delete_event')]
+    public function delete_event($id, EvenementRepository $repoEvent)
+    {
+        if ($repoEvent->deleteById($id)) {
+            echo ("Supprimé avec succes");
         };
     }
 }
